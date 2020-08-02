3e3e2f5cb85c3901d8f8aacc33f8a488d44134c8
ovitaliy <ovitaliy.rudenko@gmail.com>
2020-08-02 21:05:57 +0300

    Add drag icon to shopping list

--- lib/pages/main/items_list/list_item_item.dart
+++ lib/pages/main/items_list/list_item_item.dart
@@ -6,19 +6,20 @@ import 'package:shared_shopping_list/pages/main/user_list/user_lists_view.dart';
 
 import '../choice.dart';
 
+const dragIcon = Icon(Icons.drag_handle, color: Colors.black26);
+
 class ListItemItem extends StatelessWidget {
   final ShoppingItem item;
   final ItemAction<ShoppingItem> delete;
   final ItemAction<ShoppingItem> rename;
   final Function(ShoppingItem, bool) onItemChanged;
 
-  const ListItemItem(
-    this.item,
-    this.delete,
-    this.rename,
-    this.onItemChanged, {
-    Key key,
-  }) : super(key: key);
+  const ListItemItem(this.item,
+      this.delete,
+      this.rename,
+      this.onItemChanged, {
+        Key key,
+      }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -27,41 +28,38 @@ class ListItemItem extends StatelessWidget {
 
     final color = item.done ? Colors.black45 : defaultTextStyle.color;
     final decoration =
-        item.done ? TextDecoration.lineThrough : TextDecoration.none;
+    item.done ? TextDecoration.lineThrough : TextDecoration.none;
 
-    return Card(
-      child: Row(
-        children: [
-          Expanded(
-            child: CheckboxListTile(
-              contentPadding: EdgeInsets.all(0),
-              controlAffinity: ListTileControlAffinity.leading,
-              value: item.done,
-              onChanged: (value) => onItemChanged(item, value),
-              title: Text(
-                item.value,
-                style: defaultTextStyle.copyWith(
-                  color: color,
-                  decoration: decoration,
+    return ReorderableListener(
+      child: Card(
+        child: Row(
+          children: [
+            Expanded(
+              child: CheckboxListTile(
+                contentPadding: EdgeInsets.all(0),
+                controlAffinity: ListTileControlAffinity.leading,
+                value: item.done,
+                onChanged: (value) => onItemChanged(item, value),
+                title: Text(
+                  item.value,
+                  style: defaultTextStyle.copyWith(
+                    color: color,
+                    decoration: decoration,
+                  ),
                 ),
               ),
             ),
-          ),
-          ReorderableListener(
-            child: Container(
-                alignment: Alignment.centerLeft,
-                child: Icon(
-                  Icons.drag_handle,
-                  color: Colors.black12,
-                )),
-          ),
-          PopupMenuButton<Choice>(
-            onSelected: (v) => v.action(),
-            itemBuilder: (BuildContext context) {
-              return getChoices(context, item).map(buildMoreMenuItem).toList();
-            },
-          )
-        ],
+            dragIcon,
+            PopupMenuButton<Choice>(
+              onSelected: (v) => v.action(),
+              itemBuilder: (BuildContext context) {
+                return getChoices(context, item)
+                    .map(buildMoreMenuItem)
+                    .toList();
+              },
+            )
+          ],
+        ),
       ),
     );
   }
